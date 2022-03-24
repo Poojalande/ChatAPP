a5a2eecf5de6986c13b85fc53c8b4dd7672b4d02
pooja lande <pooja.lande@neosoftmail.com>
2022-03-24 20:41:42 +0530

    firebase setup for android and ios

--- ios/chatAPP/AppDelegate.m
+++ ios/chatAPP/AppDelegate.m
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -48,7 +48,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
+  [FIRApp configure]
   [RNSplashScreen show];
   return YES;
 }
